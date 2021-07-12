885a142526b769aa31f03717ab0386d866db75ba
Bastien <bastien.patureau@yahoo.com>
2021-07-12 14:48:19 +0200

    js form

--- form.php
+++ form.php
@@ -1,32 +1,17 @@
-<form method="post" action="form">
+<form method="post" class="form" action="form">
 <div class="field">
   <label class="label">raison de votre visite</label>
   <div class="control">
-    <select name="raison" id="raison">
+    <select name="raison" class="form_raison" id="raison">
       <option value="selectionnez">selectionnez</option>
       <option value="formation">Suivre une formation</option>
-      <option value="Rdv">Rendez-vous avec un membre du personnel</option>
+      <option value="rdv">Rendez-vous avec un membre du personnel</option>
     </select>
   </div>
 </div>
+<div class="field complement">
 
-<div class="field">
-  <label class="label">qui souhaitez vous rencontrer</label>
-  <div class="control">
-    <input class="input" name="qui" type="text" placeholder="Adam Smith">
-  </div>
 </div>
-<div class="field">
-  <label class="label">Formation que vous suivez</label>
-  <div class="control">
-  <select name="formation" id="raison">
-      <option value="ux">UX/UI les bests</option>
-      <option value="front">Front-end les boss</option>
-      <option value="back">back-end les buses</option>
-    </select>
-  </div>
-</div>
-
     <input type="hidden" name="add_user">
     <button class="button is-primary">Confirmer</button>
 </form>
--- script/form.js
+++ script/form.js
@@ -1,5 +1,34 @@
-
-
+let form = document.querySelector(".form")
+let complement = document.querySelector(".complement")
+form.addEventListener("change", e => {
+  e.preventDefault;
+  if(e.target.classList.contains("form_raison")){
+    if(e.target.value == "formation"){
+      complement.innerHTML = 
+      `
+      <label class="label">Qui souhaitez vous rencontrer</label>
+      <div class="control">
+        <input class="input" name="qui" type="text" placeholder="Adam Smith">
+      </div>
+      `
+    }else if(e.target.value == "rdv"){
+      complement.innerHTML = 
+      `
+      <label class="label">Formation que vous suivez</label>
+      <div class="control">
+      <select name="formation" id="raison">
+          <option value="ux">UX/UI les bests</option>
+          <option value="front">Front-end les boss</option>
+          <option value="back">back-end les buses</option>
+        </select>
+      </div>
+      `
+    }
+    else{
+      complement.innerHTML = ``
+    }
+  }
+})
 
 
 fetch(`https://firestore.googleapis.com/v1/projects/ingrwf09/databases/(default)/documents/${cible}`) //cible = personnel | personnel/id | cours | cours/id | planning | planning/id
